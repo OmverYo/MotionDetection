2dd77c2f297d128cdb52bbba5662010337934f09
OmverYo <64951757+OmverYo@users.noreply.github.com>
2024-01-08 11:03:27 +0900

    손 좌표값 API

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify
 import cv2
 import mediapipe as mp
 import numpy as np
+import poseModule as pm
 
 mp_pose = mp.solutions.pose
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
@@ -12,6 +13,9 @@ app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
 def openMain():
+
+    detector = pm.poseDetector()
+
     with mp_selfie_segmentation.SelfieSegmentation(model_selection = 0) as selfie_segmentation, mp_pose.Pose(model_complexity = 0, min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose:
         # 배경화면의 색상을 지정합니다
         BG_COLOR = (0, 255, 0) # GREEN
@@ -24,8 +28,10 @@ def openMain():
                 break
 
             else:
+                image = detector.findPose(image)
+                results1 = detector.findHand(image)
+
                 results = selfie_segmentation.process(image)
-                results1 = pose.process(image)
 
                 condition = np.stack((results.segmentation_mask,) * 3, axis = -1) > 0.15
 
@@ -37,18 +43,24 @@ def openMain():
                 # 결과 이미지를 적용합니다
                 output_image = np.where(condition, image, bg_image)
 
-                ret, buffer = cv2.imencode('.jpg', output_image)
-                output_image = buffer.tobytes()
-                yield (b'--image\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + output_image + b'\r\n')
+                return results1
 
-@app.route('/main')
+                # ret, buffer = cv2.imencode('.jpg', output_image)
+                # output_image = buffer.tobytes()
+                # yield (b'--image\r\n'
+                #     b'Content-Type: image/jpeg\r\n\r\n' + output_image + b'\r\n')
+
+@app.route('/main', methods=['GET'])
 def main():
-    return render_template('main.html')
+    return jsonify(openMain())
 
-@app.route('/video_main')
-def video_main():
-    return Response(openMain(), mimetype='multipart/x-mixed-replace; boundary=image')
+# @app.route('/main')
+# def main():
+#     return render_template('main.html')
+
+# @app.route('/video_main')
+# def video_main():
+#     return Response(openMain(), mimetype='multipart/x-mixed-replace; boundary=image')
 
 if __name__ == '__main__':
     app.run(debug=True)
